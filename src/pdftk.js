8d26bfa54aaffc69003c533c0a53c970ca08c966
Peter Nagel <pnagel5@gmail.com>
2014-05-29 19:32:43 -0700

    moved check cmd to utils

--- src/pdftk.js
+++ src/pdftk.js
@@ -11,24 +11,4 @@ var Pdftk = function () {
   return this.checkCmdExists('pdftk').done();
 };
 
-/*
- * Check if the supplied command exists in the $PATH
- * @author - Peter Nagel
- * @date - 5/29/2014
- * @param {string} - cmd to check for existence
-*/
-Pdftk.prototype.checkCmdExists = function(cmd) {
-  var checkCmd = 'type -P ' + cmd + ' &>/dev/null && echo 1 || echo 0';
-  return pexec(checkCmd)
-    .then(function (stdout, stderr) {
-      if (stdout && stdout[0].substring(0,1) === '1') {
-        return true;
-      } else if (stdout && stdout[0].substring(0,1) === '0') {
-        throw new errors.CmdDoesNotExist();
-      } else {
-        throw new errors.CheckCmdFailed();
-      }
-    });
-};
-
 module.exports = Pdftk;
--- test/unit.js
+++ test/unit.js
@@ -1,30 +1,27 @@
-var pdftkFactory = require('../src');
 var chai         = require('chai');
 var expect       = chai.expect;
 var errors       = require('../src/errors');
+var utils        = require('../src/utils');
 
 chai.use(require('chai-as-promised'));
 
 describe('Pdftk: Unit', function () {
-  var pdftk;
-  beforeEach(function () {
-    pdftk = new pdftkFactory();
-  });
-
-  describe('#checkCmdExists', function () {
-    it('returns rejected promise for a cmd that does not exist', function () {
-      expect(pdftk.checkCmdExists('asdfasdf'))
-        .to.be.rejectedWith(errors.CmdDoesNotExist);
-    });
-    it('returns fulfilled promise for a cmd that does exist', function () {
-      return pdftk.checkCmdExists('ls')
-      .then(function (response) {
-        expect(response).to.equal(true);
+  describe('utils', function () {
+    describe('#checkCmdExists', function () {
+      it('returns rejected promise for a cmd that does not exist', function () {
+        expect(utils.checkCmdExists('asdfasdf'))
+          .to.be.rejectedWith(errors.CmdDoesNotExist);
       });
-    });
-    it('returns rejected promise for an unexpected result', function () {
-      expect(pdftk.checkCmdExists('ls -l echo &&'))
-        .to.be.rejectedWith(errors.CheckCmdFailed);
+      it('returns fulfilled promise for a cmd that does exist', function () {
+        return utils.checkCmdExists('ls')
+        .then(function (response) {
+          expect(response).to.equal(true);
+        });
+      });
+      it('returns rejected promise for an unexpected result', function () {
+        expect(utils.checkCmdExists('ls -l echo &&'))
+          .to.be.rejectedWith(errors.CheckCmdFailed);
+      });
     });
   });
 });
